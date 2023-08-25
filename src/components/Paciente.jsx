6f38303a9b376286559e761fe7f32b2edd7ce1d6
Andrea Torres <atorres@cellforce.com>
2023-08-25 13:37:46 -0500

    Adding initial components and styles

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,27 @@
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase"> Nombre: {''}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase"> Propietario: {''}
+        <span className="font-normal normal-case">Andrea</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase"> Email: {''}
+        <span className="font-normal normal-case">correo@example.com</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase"> Fecha Alta: {''}
+        <span className="font-normal normal-case">10 Diciembre de 2023</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase"> Síntomas: {''}
+        <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat incidunt ipsa similique voluptatibus aliquid, illo minus velit. Qui nobis illum velit quam! Earum, voluptatum optio nostrum beatae dolore eos consequuntur?</span>
+      </p>
+    </div>
+  )
+}
+
+export default Paciente
